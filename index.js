890d81623abce0d34c2b2c6b9ace5c0e487ef743
Abdullah Afzal <abdullah941685@gmail.com>
2024-12-29 15:30:46 +0500

    ab

--- index.js
+++ index.js
@@ -12,11 +12,14 @@ connectDb()
 app.use(express.json())
 app.use(cors())
 
+app.get('/', () => {
+    console.log("vercel is working")
+})
 
-// const { PORT = 8000 } = process.env;
+const { PORT = 8000 } = process.env;
 
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`)
-// })
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
 app.use("/auth", authRouter)
 app.use("/", notesRouter)
